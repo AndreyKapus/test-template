dc27d8008cf068b4554839514167aa98c9f146f2
AndreyKapus <kapustnikov@ukr.net>
2023-10-26 19:28:39 +0300

    add project

--- src/Components/Layout/AddProject/AddProject.tsx
+++ src/Components/Layout/AddProject/AddProject.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import { AddProjectBtn } from "./AddProject.styled";
 
 const AddProject = () => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const screenWidth = window.screen.width;
+    setWidth(screenWidth);
+  }, [width]);
+
   return (
     <>
-      <AddProjectBtn type="button">Add project</AddProjectBtn>
+      {width < 768 ? (
+        <AddProjectBtn type="button">Add project</AddProjectBtn>
+      ) : (
+        <div>New project</div>
+      )}
     </>
   );
 };
